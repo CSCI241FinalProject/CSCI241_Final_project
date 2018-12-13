8df53a8eb51194ee5720fb561c7f8e0e21ddd6ce
gaurab278 <gpokhare@oberlin.edu>
2018-12-13 15:23:03 -0500

    Some Pathches  to AI behavior.

--- CSCI241_Checkers/Assets/Script/AI_Behavior.cs
+++ CSCI241_Checkers/Assets/Script/AI_Behavior.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 public class AI_Behavior : CheckersBoard
 {
     //Define recursive depth
-    readonly int DEPTH = 4; 
+    readonly int DEPTH = 4;
 
     //Define the offensive points 
     readonly int CAPTUREPIECE = 2;
@@ -19,7 +21,6 @@ public class AI_Behavior : CheckersBoard
     readonly int KINGATRISK = -4;
 
 
-
     //Function that returns all the black pieces on the board as a list
     public List<Piece> GetBlackPieces(Piece[,] board)
     {
@@ -29,17 +30,85 @@ public class AI_Behavior : CheckersBoard
         {
             for (int y = 0; y < 8; y++)
             {
-                if ((board[x,y] != null) &&(!board[x, y].isWhite))
+                if ((board[x, y] != null) && (board[x, y].isWhite == false))
                 {
                     result.Add(board[x, y]);
                 }
             }
         }
+
+
+        Debug.Log("0000000000000000000000000000000000000000000000000000000000000000000000000000000000");
+        /*debug*/
+        Debug.Log("Black pieces:");
+        foreach (Piece x in result)
+        {
+            Debug.Log(x.x);
+            Debug.Log(x.y);
+            Debug.Log(" ");
+        }
+        Debug.Log("0000000000000000000000000000000000000000000000000000000000000000000000000000000000");
+
+
+
         return result;
     }
 
 
+    //Function that copies the board state passed in and returns new board
+    public Piece[,] CopyBoard(Piece[,] board)
+    {
+        Piece[,] result = new Piece[8, 8];
 
+
+        /*debug*/
+
+        for (int x = 0; x < 8; x++)
+        {
+            for (int y = 0; y < 8; y++)
+            {
+
+
+                if (board[x, y] != null)
+                {
+
+
+
+                    result[x, y] = board[x, y];
+
+                    /*
+                    if (board[x, y].isWhite)
+                    {
+                        GameObject current = Instantiate(whitePiecePrefab) as GameObject;
+                        current.transform.SetParent(transform);
+                        Piece putThis = current.GetComponent<Piece>();
+
+                        result[x, y] = putThis;
+                        putThis.x = x;
+                        putThis.y = y;
+                        putThis.isWhite = true;
+                        putThis.isWhite = board[x, y].isKing;
+
+                    }
+                    else
+                    {
+                        GameObject current = Instantiate(blackPiecePrefab) as GameObject;
+                        current.transform.SetParent(transform);
+                        Piece putThis = current.GetComponent<Piece>();
+
+                        result[x, y] = putThis;
+                        putThis.x = x;
+                        putThis.y = y;
+                        putThis.isWhite = false;
+                        putThis.isWhite = board[x, y].isKing;
+                    }
+                    */
+                }
+            }
+        }
+
+        return result;
+    }
 
 
 
@@ -52,35 +121,30 @@ public class AI_Behavior : CheckersBoard
         {
             for (int y = 0; y < 8; y++)
             {
-                if ((board[x, y] != null) && (board[x, y].isWhite))
+                if ((board[x, y] != null) && (board[x, y].isWhite == true))
                 {
                     result.Add(board[x, y]);
                 }
             }
         }
-        return result;
-    }
 
-
-
-
-
-
-    //Function that copies the board state passed in and returns new board
-    private Piece[,] CopyBoard(Piece[,] board)
-    {
-        Piece[,] result = new Piece[8, 8];
-
-        for (int x = 0; x < 8; x++)
+        /*debug*/
+        /*Debug.Log("white pieces:");
+        foreach (Piece x in result)
         {
-            for (int y = 0; y < 8; y++)
-            {
-                result[x, y] = board[x, y];
+            Debug.Log(x.x);
+            Debug.Log(x.y);
+            Debug.Log(" ");
+        }*/
 
-            }
-        }
+
         return result;
     }
+
+
+
+
+
 
 
 
@@ -91,6 +155,14 @@ public class AI_Behavior : CheckersBoard
     //Function to get a list of all the possible moves in the passed in board state for the piece 'p'
     private List<Piece> GetPossibleMoves(Piece p, Piece[,] board)
     {
+
+        /*Debug.Log("*************current piece:*********************");
+        Debug.Log(p.x);
+        Debug.Log(p.y);
+       */
+
+
+
         List<Piece> result = new List<Piece>();
 
 
@@ -179,6 +251,7 @@ public class AI_Behavior : CheckersBoard
                 }
             }
 
+
             return result;
         }
 
@@ -213,56 +286,65 @@ public class AI_Behavior : CheckersBoard
                 Piece n = new Piece(p.x + 1, p.y - 1);
                 result.Add(n);
             }
+
+            return result;
         }
 
+
+        //If p is not the king, then need to check two adjacent squares based on the color.
+        if (p.isWhite)
+        {
+            //if the piece is white, need to go forward, i.e the y co-ordinates increase
+            if (p.CheckMoveValidation(board, p.x, p.y, p.x - 1, p.y + 1))
+            {
+                Piece n = new Piece(p.x - 1, p.y + 1);
+                result.Add(n);
+            }
+            if (p.CheckMoveValidation(board, p.x, p.y, p.x + 1, p.y + 1))
+            {
+                Piece n = new Piece(p.x + 1, p.y + 1);
+                result.Add(n);
+            }
+            return result;
+        }
         else
         {
-            //If p is not the king, then need to check two adjacent squares based on the color.
-
-            if (p.isWhite)
+            //if the piece is black, need to go backward, i.e. the y co-ordinates decrease 
+            if ((p.x - 1 >= 0) && (p.y - 1 >= 0) && (p.x - 1 <= 7) && (p.y - 1 <= 7))
             {
-                //if the piece is white, need to go forward, i.e the y co-ordinates increase
-                if (p.CheckMoveValidation(board, p.x, p.y, p.x - 1, p.y + 1))
-                {
-                    Piece n = new Piece(p.x - 1, p.y + 1);
-                    result.Add(n);
-                }
-                if (p.CheckMoveValidation(board, p.x, p.y, p.x + 1, p.y + 1))
-                {
-                    Piece n = new Piece(p.x + 1, p.y + 1);
-                    result.Add(n);
-                }
-            }
-            else
-            {
-                //if the piece is black, need to go backward, i.e. the y co-ordinates decrease 
-                if (p.CheckMoveValidation(board, p.x, p.y, p.x - 1, p.y - 1))
+                if (board[p.x - 1, p.y - 1] == null)
                 {
                     Piece n = new Piece(p.x - 1, p.y - 1);
                     result.Add(n);
                 }
-                if (p.CheckMoveValidation(board, p.y, p.y, p.x + 1, p.y - 1))
+            }
+
+            if ((p.x + 1 >= 0) && (p.y - 1 >= 0) && (p.x + 1 <= 7) && (p.y - 1 <= 7))
+            {
+                if (board[p.x + 1, p.y - 1] == null)
                 {
                     Piece n = new Piece(p.x + 1, p.y - 1);
                     result.Add(n);
                 }
-
             }
+            return result;
         }
-        return result;
+
+
+
     }
 
 
 
 
-   
+
 
 
     //Function to carry out a virtual move in the virtual board 
     private Move MakeVirtualMove(Piece source, Piece dest, Move thisMove)
     {
         //TODO make score calculation here.
-        int newScore = 0; 
+        int newScore = 0;
 
 
         //Make a copy of the board that has been passed in
@@ -286,7 +368,7 @@ public class AI_Behavior : CheckersBoard
                 Piece dead = newBoard[((x1 + x2) / 2), ((y1 + y2) / 2)]; //position of dead piece
                 if (dead != null)
                 {
-                    boardPieces[((x1 + x2) / 2), ((y1 + y2) / 2)] = null;
+                    newBoard[((x1 + x2) / 2), ((y1 + y2) / 2)] = null;
                 }
             }
 
@@ -301,12 +383,9 @@ public class AI_Behavior : CheckersBoard
 
 
         Move result = new Move(thisMove.GetOrigin(), thisMove.GetDestination(), newBoard);
-        result.score = newScore; 
+        result.score = newScore;
         return result;
     }
-
-
-
 
 
 
@@ -330,7 +409,7 @@ public class AI_Behavior : CheckersBoard
         //loop through the list of final board states and return the move with the max score 
         foreach (Move move in list)
         {
-            if (move.score > result.score)
+            if (move.score >= result.score)
             {
                 result = move;
             }
@@ -339,104 +418,194 @@ public class AI_Behavior : CheckersBoard
     }
 
 
-    private void ChildMove(Move move, bool isWhite, int depth) {
+    private void ChildMove(Move move, bool isWhite, int depth)
+    {
 
-      
 
         //base case. add all the children moves into the global variable and return  
-        if (depth <= 0) {
+        if (depth <= 0)
+        {
             finalList.Add(move);
             return;
         }
 
 
-        //For each board state present in move.children
-        foreach (Move Mover in move.GetChildren())
+
+
+        Piece[,] board = move.GetBoard();
+
+        //Create a list of pieces of current color
+        List<Piece> pieces = new List<Piece>();
+        if (isWhite)
+        {
+            pieces = GetWhitePieces(board);
+        }
+        else
+        {
+            pieces = GetBlackPieces(board);
+        }
+
+
+        List<Move> newList = new List<Move>(); //create new list to set the children of each of this current move
+
+
+        //for each piece of the given color
+        foreach (Piece origin in pieces)
         {
 
-            Piece[,] board = Mover.GetBoard(); 
+            //Generate a list of all possible moves
+            List<Piece> destinations = GetPossibleMoves(origin, board);
 
-            //Create a list of pieces of current color
-            List<Piece> pieces = new List<Piece>();
-            if (isWhite)
+            //Once you have a list of all possible moves. Carry out the virtual move on the board
+            foreach (Piece destination in destinations)
             {
-                pieces = GetWhitePieces(board);
-            }
-            else
-            {
-                pieces = GetBlackPieces(board);
-            }
-
-
-            List<Move> newList = new List<Move>(); //create new list to set the children of each of this current move
-
-
-            //for each piece of the given color
-            foreach (Piece origin in pieces)
-            { 
-                //Generate a list of all possible moves
-                List<Piece> destinations = GetPossibleMoves(origin, board);
-
-                //Once you have a list of all possible moves. Carry out the virtual move on the board
-                foreach (Piece destination in destinations)
-                {
-                    //Make virtual move here and call the recursive function on the following: 
-                    Move newMove = MakeVirtualMove(origin, destination, Mover);
-                    newList.Add(newMove);
-                }
-            }
-            Mover.SetChildren(newList);
-
-            //call recursive function on each move in newList with depth-1, oppposite color.
-            foreach (Move n in newList) {
-                ChildMove(n, !isWhite, depth--);
+                //copy the board
+                Piece[,] newBoard = CopyBoard(board);
+                //make a new move and carry out a virual move on that board 
+                Move newM = new Move(move.GetOrigin(), move.GetDestination(), newBoard);
+                //Make virtual move here and call the recursive function on the following: 
+                Move newMove = MakeVirtualMove(origin, destination, newM);
+                newList.Add(newMove);
             }
         }
+
+
+        //call recursive function on each move in newList with depth-1, oppposite color.
+        foreach (Move n in newList)
+        {
+            ChildMove(n, !isWhite, depth--);
+        }
+
 
     }
 
 
+    private List<Piece> blackPieces;
 
 
-    public int[] GetMove(Piece[,] board) {
+    public int[] GetMove(Piece[,] mainBoard)
+    {
         int[] result = new int[4];
 
-        List<Piece> blackPieces = GetBlackPieces(board); //get all black pieces on the board
+
+        Piece[,] board = CopyBoard(mainBoard);
+
+
+
+        /*debug*/
+        /*
+        for (int x = 0; x < 8; x++)
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                if (board[x, y] != null)
+                {
+                    print(board[x, y].x);
+                    print(board[x, y].y);
+
+
+                    if (board[x, y].isWhite)
+                    {
+                        print("ISWhite");
+                    }
+                    else
+                    {
+                        print("ISBlack");
+                    }
+
+                    print(" ");
+
+                }
+                else
+                {
+                    print("NULL");
+                    print(" ");
+                }
+            }
+        }
+
+        */
+
+
+
+
+
+
+
+        blackPieces = GetBlackPieces(board); //get all black pieces on the board
         List<Move> moves = new List<Move>();//list to store the list of moves
 
         //For each black piece on the board
-        foreach (Piece piece in blackPieces) {
+        foreach (Piece piece in blackPieces)
+        {
+
+            Debug.Log("*************current piece:*********************");
+            Debug.Log(piece.x);
+            Debug.Log(piece.y);
+
 
             //get a list of all possible destinations
             List<Piece> destinations = GetPossibleMoves(piece, board);
 
-            //for each destination
-            foreach (Piece destination in destinations) {
 
+            Debug.Log("--------------------------------------------");
+            Debug.Log("destinations:");
+
+
+
+            //for each destination1
+            foreach (Piece destination in destinations)
+            {
+                Debug.Log(destination.x);
+                Debug.Log(destination.y);
+
+
+                Piece[,] newBoard = CopyBoard(board);
                 //make a new move and carry out a virual move on that board 
-                Move newMove = new Move(piece, destination, board);
+
+
+                //generate a phantom piece for the origin 
+                Piece origin = new Piece(piece.x, piece.y);
+
+                Move newMove = new Move(origin, destination, newBoard);
                 Move resultantMove = MakeVirtualMove(piece, destination, newMove);
                 moves.Add(resultantMove); //add the virtual move into the board
             }
+
+            Debug.Log("#########################################");
         }
 
         //now call the recursive function on each of the child moves carried out 
-        foreach (Move thismove in moves) {
-            ChildMove(thismove, true, 3); 
+        foreach (Move thismove in moves)
+        {
+            ChildMove(thismove, true, 4);
         }
 
 
         //do the resetting, and returning here
 
         Move finalMove = GetBestMove(finalList); //getting the best move among the list of moves
-        finalList = new List<Move>(); //resetting the global variable 
+        finalList = null; //resetting the global variable 
 
         result[0] = finalMove.GetOrigin().x;
         result[1] = finalMove.GetOrigin().y;
         result[2] = finalMove.GetDestination().x;
-        result[3] = finalMove.GetDestination().y; 
+        result[3] = finalMove.GetDestination().y;
 
-        return result; 
+
+        Debug.Log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        Debug.Log("BestMove: ");
+        Debug.Log(result[0]);
+        Debug.Log(result[1]);
+        Debug.Log("TO: ");
+        Debug.Log(result[2]);
+        Debug.Log(result[3]);
+        Debug.Log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+
+
+
+
+        return result;
     }
 
 
